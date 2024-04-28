dcc961f9873e73e0fbb106d535a8be2b1902674b
Nadeesh Peiris <nash.ucsc@gmail.com>
2024-04-28 08:49:06 +0530

    add axios-oauth-client

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -1,12 +1,29 @@
 import axios from "axios";
+import oauth from 'axios-oauth-client';
 
 declare global {
   interface Window {
     configs: {
       apiUrl: string;
+      consumerKey: string;
+      consumerSecret: string;
+      tokenUrl: string;
     };
   }
 }
+
+// consumerKey, consumerSecret and tokenUrl represent variables to which respective environment variables were read
+const getClientCredentials = oauth.clientCredentials(
+  axios.create({
+    baseURL: window?.configs?.apiUrl || "http://localhost:3000/",
+  }),
+  window?.configs?.tokenUrl,
+  window?.configs?.consumerKey,
+  window?.configs?.consumerSecret
+);
+
+const auth = await getClientCredentials('');
+const accessToken = auth.access_token;
 
 // create axios instance
 const api = axios.create({
@@ -14,6 +31,10 @@ const api = axios.create({
 });
 
 export const fetchContests = async () => {
-  const response = await api.get("/contests");
+  const response = await api.get("/contests",{
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+});
   return response.data;
 };
